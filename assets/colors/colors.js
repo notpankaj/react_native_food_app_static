698cab6810e369c9f04089682d2b2189ae3386a2
notpankaj <iampankaj0409@gmail.com>
2022-08-07 18:49:06 +0530

    init

---
+++ assets/colors/colors.js
@@ -0,0 +1,12 @@
+const colors = {
+  background: '#F9F9FB',
+  primary: '#F5CA48',
+  secondary: '#F26C68',
+  textDark: '#313234',
+  textLight: '#CDCDCD',
+  price: '#E4723C',
+  white: '#fff',
+  black: '#000',
+};
+
+export default colors;
